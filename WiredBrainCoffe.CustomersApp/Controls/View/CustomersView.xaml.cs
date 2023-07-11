25f77cc06155bf61113b897296e8d5c36d963a9a
jmalagonnavarro <juan.malagonnavarro@amadeus.com>
2023-07-11 11:34:07 -0500

    Initial commit

---
+++ WiredBrainCoffe.CustomersApp/Controls/View/CustomersView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WiredBrainCoffe.CustomersApp.Controls.View
+{
+    /// <summary>
+    /// Interaction logic for CustomersView.xaml
+    /// </summary>
+    public partial class CustomersView : UserControl
+    {
+        public CustomersView()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonMoveNavigation_Clcik(object sender, RoutedEventArgs e)
+        {
+            //int column = (int) customerListGrid.GetValue(Grid.ColumnProperty);
+            //int newColumn = column == 0 ? 2 : 0;
+
+            //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            int column = Grid.GetColumn(customerListGrid);
+            int newColumn = column == 0 ? 2 : 0;
+
+            Grid.SetColumn(customerListGrid, newColumn);
+        }
+    }
+}
